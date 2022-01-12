deb4c04114c84613b9e00b4696c86e20c64a8fce
KhanhNguyen1102 <sukhanh12@gmail.com>
2022-01-12 09:31:46 +0700

    controller

---
+++ src/main/java/com/example/test_module4/controller/CityRestController.java
@@ -0,0 +1,60 @@
+package com.example.test_module4.controller;
+
+import com.example.test_module4.model.City;
+import com.example.test_module4.service.ICityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api/cities")
+public class CityRestController {
+    @Autowired
+    private ICityService cityService;
+    @GetMapping
+    public ResponseEntity<Iterable<City>> findAllCity() {
+        List<City> cities = (List<City>) cityService.findAll();
+        if (cities.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(cities, HttpStatus.OK);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<City> findCityById(@PathVariable Long id) {
+        Optional<City> cityOptional = cityService.findById(id);
+        if (!cityOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        City city = cityOptional.get();
+        return new ResponseEntity<>(city, HttpStatus.OK);
+    }
+    @PostMapping
+    public ResponseEntity<City> saveCity(@RequestBody City city) {
+        cityService.save(city);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<City> updateCity(@PathVariable Long id, @RequestBody City city) {
+        Optional<City> cityOptional = cityService.findById(id);
+        if (!cityOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        city.setId(cityOptional.get().getId());
+        cityService.save(city);
+        return new ResponseEntity<>( HttpStatus.OK);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<City> deleteCity(@PathVariable Long id) {
+        Optional<City> cityOptional = cityService.findById(id);
+        if (!cityOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        cityService.remove(id);
+        return new ResponseEntity<>(cityOptional.get(), HttpStatus.NO_CONTENT);
+    }
+}
